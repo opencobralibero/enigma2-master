bb05542d5abbb5e4b0a43fa0b0b2e5fd0d29830d
opencobralibero <63002173+opencobralibero@users.noreply.github.com>
2021-06-15 09:33:58 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -28,8 +28,8 @@ class About(Screen):
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
-		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
-		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
+		AboutText += _("Info: www.cobralibero.net ") + about.getBuildDateString() + "\n"
+		AboutText += _("Creata: da ^^enri74^^ & ricorico ") + about.getUpdateDateString() + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
